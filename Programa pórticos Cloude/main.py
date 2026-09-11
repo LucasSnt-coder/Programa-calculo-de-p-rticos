cb4bd0889db5e342648e11011d32d21e107fd4b4
LucasSnt-coder <san17joro@gmail.com>
2026-09-11 08:20:30 -0500

    Add files via upload

---
+++ Programa pórticos Cloude/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+main.py
+-------
+Punto de entrada del software de Analisis de Estructuras 2D
+(Porticos y Armaduras) por el Metodo Directo de la Rigidez.
+
+Ejecutar con:
+    python main.py
+
+Requisitos (ver requirements.txt):
+    customtkinter, matplotlib, numpy
+"""
+
+from app_interface import run_app
+
+if __name__ == "__main__":
+    run_app()
